0137d998f38f2732ff0d9d5ec73e8b3eb6c850f9
gjinie <yaf2009@naver.com>
2025-08-02 12:26:13 +0900

    히히 주석달았다

--- chap07/sec01/gpt_functions.py
+++ chap07/sec01/gpt_functions.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pytz
-
+#####w주석...
 def get_current_time(timezone: str = 'Asia/Seoul'):
     tz = pytz.timezone(timezone) # 타임존 설정
     now = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
